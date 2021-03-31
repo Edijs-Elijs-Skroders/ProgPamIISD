8ae811e3b86cc85772832a53ad43035f9ff47594
Edijs Elijs Skroders <edijsskroders@gmail.com>
2021-04-01 01:04:33 +0300

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public abstract class Main implements MouseListener {
+
+    public static int column = 8;
+    public static int row = 8;
+    public static Color clr1 = Color.WHITE;
+    public static Color clr2 = new Color(74, 96, 134);
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+
+        //Jframe uzstādījumi.
+
+        JFrame checkers = new JFrame("Kursa Darbs");
+        checkers.setSize(820, 950);
+        ImageIcon ckrsIcon = new ImageIcon("res/tick.png");
+        checkers.setIconImage(ckrsIcon.getImage());//nomaina ikonu
+
+        //Izveido JPanel ar 64 panels specifiskās krāsās.
+
+        JPanel board = new JPanel();
+        board.setLayout(new GridLayout(row, column));
+        Color c;
+
+        for (int a = 0; a < row; a++) {
+            if (a % 2 == 0) {
+                c = clr1;
+            } else c = clr2;
+
+            for (int b = 0; b < column; b++) {
+                JPanel square = new JPanel();
+                square.setBackground(c);
+                if (c.equals(clr1)) {
+                    c = clr2;
+                } else c = clr1;
+                board.add(square);
+            }
+        }
+
+        board.setBorder(BorderFactory.createLineBorder(Color.BLACK)); //Laukumam uzstāda malu, lai labāk uztvertu
+        board.setBounds(5, 100, 750, 750);
+
+        JPanel settings = new JPanel();
+        settings.setLayout(new FlowLayout());
+        Button newGame = new Button("New Game");
+        settings.add(newGame);
+        settings.setBounds(0, 0, 100, 100);
+
+        checkers.add(board);
+        checkers.add(settings);
+        checkers.setVisible(true);
+        checkers.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
